f1aa5faed0725cc89e8b3ba7b303265beadc0639
asimgeek <asim2018off@gmail.com>
2023-08-30 18:44:36 +0530

    Intialize

---
+++ app/src/main/java/com/example/symptocareai/Home.kt
@@ -0,0 +1,28 @@
+package com.example.symptocareai
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+class Home : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        auth = FirebaseAuth.getInstance()
+
+        val email = intent.getStringExtra("email")
+        val displayName = intent.getStringExtra("name")
+
+        findViewById<TextView>(R.id.textView).text = email + "\n" + displayName
+
+        findViewById<Button>(R.id.googleSignOutButton).setOnClickListener{
+            auth.signOut()
+            startActivity(Intent(this, Login::class.java))
+        }
+    }
+}
