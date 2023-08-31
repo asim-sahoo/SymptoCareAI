caaa94c7f62e3a36b1010f22adc981a46fe7bb57
asimgeek <asim2018off@gmail.com>
2023-08-31 15:09:50 +0530

    Added Comments

--- app/src/main/java/com/example/symptocareai/Login.kt
+++ app/src/main/java/com/example/symptocareai/Login.kt
@@ -25,26 +25,32 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // Initialize Firebase Authentication instance
         auth = FirebaseAuth.getInstance()
 
+        // Set up Google Sign-In options
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
+            .requestIdToken(getString(R.string.default_web_client_id)) // Configure Google client ID
+            .requestEmail() // Request user's email
             .build()
 
+        // Create GoogleSignInClient
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
+        // Set click listener for Google Sign-In button
         findViewById<Button>(R.id.googleSignInButton).setOnClickListener {
             signInGoogle()
         }
     }
 
     private fun signInGoogle() {
+        // Check if the user is already signed in and clear cached account if needed
         val signedInAccount = GoogleSignIn.getLastSignedInAccount(this)
         if (signedInAccount != null) {
-            googleSignInClient.signOut() // Clear cached account
+            googleSignInClient.signOut()
         }
 
+        // Launch the Google Sign-In intent
         val signInIntent = googleSignInClient.signInIntent
         launcher.launch(signInIntent)
     }
@@ -63,19 +69,25 @@ class Login : AppCompatActivity() {
                 updateUI(account)
             }
         } else {
+            // Display error message if sign-in task fails
             Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun updateUI(account: GoogleSignInAccount) {
+        // Create Google Auth credentials using the account's ID token
         val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+        // Sign in to Firebase with the provided credentials
         auth.signInWithCredential(credential).addOnCompleteListener {
             if (it.isSuccessful) {
+                // If sign-in is successful, navigate to the Home activity and pass user data
                 val intent: Intent = Intent(this, Home::class.java)
                 intent.putExtra("email", account.email)
                 intent.putExtra("name", account.displayName)
                 startActivity(intent)
             } else {
+                // Display error message if Firebase authentication fails
                 Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
             }
         }
